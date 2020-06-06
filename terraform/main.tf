bda563cbd3f9c578a746b587a6ec4a7acdd7e5dc
luanngominh <luannm2@vng.com.vn>
2020-06-07 00:56:18 +0700

    terraform provision vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,69 @@ resource "aws_vpc" "ceph-vpc" {
 }
 
 
+resource "aws_subnet" "ceph-vpc-public" {
+  vpc_id = "${aws_vpc.ceph-vpc.id}"
+  cidr_block = "1.13.1.0/24"
+
+  tags = {
+    Name = "ceph-vpc-public"
+  }
+
+  lifecycle {
+    create_before_destroy = false
+  }
+}
+
+resource "aws_internet_gateway" "ceph-vpc-igw" {
+  vpc_id = "${aws_vpc.ceph-vpc.id}"
+
+  tags = {
+    Name = "ceph-vpc-igw"
+  }
+}
+
+resource "aws_route_table" "ceph-vpc-public-route" {
+  vpc_id = "${aws_vpc.ceph-vpc.id}"
+
+  route {
+    cidr_block        = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ceph-vpc-igw.id}"
+  }
+
+  tags = {
+    Name = "ceph-vpc-public-route"
+  }
+}
+
+resource "aws_security_group" "allow-all-traffic-local" {
+  name        = "allow-all-traffic-local"
+  description = "Allow all traffic local"
+  vpc_id      = "${aws_vpc.ceph-vpc.id}"
+
+  ingress {
+    description = "All traffic from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.ceph-vpc.cidr_block]
+  }
+
+  ingress {
+    description = "SSH from others"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-all-traffic-local"
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,15 @@
 output "ceph_vpc_id" {
   value = aws_vpc.ceph-vpc.id
 }
+
+output "ceph_vpc_public_subnet" {
+  value = aws_subnet.ceph-vpc-public.id
+}
+
+output "ceph_vpc_interget_gateway" {
+  value = aws_internet_gateway.ceph-vpc-igw.id
+}
+
+output "ceph_vpc_security_group" {
+  value = aws_security_group.allow-all-traffic-local.id
+}
